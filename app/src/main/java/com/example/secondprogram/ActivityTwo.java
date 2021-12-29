f1f68273ad757d2c715a99d4b3d1a4196abb48d9
danielbugdari <71017119+danielbugdari@users.noreply.github.com>
2021-12-29 21:01:44 +0200

    test

--- app/src/main/java/com/example/secondprogram/ActivityTwo.java
+++ app/src/main/java/com/example/secondprogram/ActivityTwo.java
@@ -2,13 +2,30 @@ package com.example.secondprogram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class ActivityTwo extends AppCompatActivity {
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_two);
+
+        textView=findViewById(R.id.textViewResult);
+        textView.setText("");
+
+        Intent intent = getIntent();
+        String str = intent.getStringExtra("key");
+        textView.setText(str);
+
+    }
+
+    public void funcBack(View view) {
+        Intent intent = new Intent(this , MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/secondprogram/MainActivity.java
+++ app/src/main/java/com/example/secondprogram/MainActivity.java
@@ -6,13 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     TextView textView;
     char opp;
     int num1;
     int num2;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
 
         textView = findViewById(R.id.textFurmola);
         textView.setText("");
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
     public void funcNumber(View view) {
@@ -34,14 +41,12 @@ public class MainActivity extends AppCompatActivity {
 
         Button b = (Button) view;
         String str = b.getText().toString();
-        //textView.setText(str);
         opp = str.charAt(0);
         num1 = Integer.parseInt(textView.getText().toString());
 
        textView.setText("");
 
     }
-
     public void funcResult(View view) {
 
         num2 = Integer.parseInt(textView.getText().toString());
@@ -64,13 +69,38 @@ public class MainActivity extends AppCompatActivity {
     public void funcClear(View view) {
 
         textView.setText("");
-
     }
 
-    public void funcGotToSec(View view) {
-
+    public void funcPushResult(View view) {
         Intent intent = new Intent(this , ActivityTwo.class);
-
+        intent.putExtra("key" ,"Hello");
         startActivity(intent);
+    }
+
+    public  void loginFunc(){
+
+        String email = ((EditText)findViewById(R.id.text1)).getText().toString();
+        String password = ((EditText)findViewById(R.id.text2)).getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+    }
+    public  void regFunc(){
+
     }
 }
